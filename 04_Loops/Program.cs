69b4c626535bfce99b0523db2bcc51baece02098
azraaabes <azra.abes@gmail.com>
2024-10-22 17:16:26 +0300

    02.Cases

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Döngüsü
+            //int i;
+            //for (i = 1; i <=5 ; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //Console.Write("Lütfen ekrana yazılmasını istediğiniz adedi giriniz: ");
+            //int finishValue = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= finishValue; i++)
+            //{
+            //    Console.WriteLine("Bugün hava güneşli");
+            //}
+
+            #endregion
+
+            #region For döngüsü ile karar yapıları
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 5 == 0) {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            //int totalvale = 0;
+            //for (int i = 1; i <=10 ; i++) { 
+            //    totalvale += i;
+            //}
+            //Console.WriteLine(totalvale);
+
+            //int totalValue = 0;
+            //for (int i = 1; i < 10; i++) {
+            //if(i % 2 == 0) { 
+            //        totalValue += i;
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+            //Console.WriteLine("-----");
+            //Console.WriteLine(totalValue);
+
+            //int count = 0;
+            //for (int i = 1; i < 50; i++) {
+            //    if (i % 7 == 0) { 
+            //    count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            //int bacterium = 1;
+            //for (int i = 1; i <=24 ; i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine(i + ".saat sonunda: " + bacterium);
+            //}
+
+
+            #endregion
+
+
+            #region While Döngüsü
+
+            //int = 1;
+            //while (int <= 10)
+            //{
+            //    Console.WriteLine("Hello World"); 
+            //    int++;
+            //}
+
+            //int i = 1;
+            //while (i <= 10) {
+            //    if (i % 3 == 0) {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+
+
+
+
+
+            #endregion
+
+
+            #region Örnek Sınav Sorusu
+
+            //int number;
+            //Console.Write("Sayıyı giriniz: ");
+            //number = int.Parse(Console.ReadLine());
+            //int ones,tens,hundres;
+            //int sum;
+
+            //ones = number % 10;
+            //hundres = number / 100; 
+            //tens = (number % 100) /10;
+
+            //sum = ones + tens + hundres;    
+            //Console.WriteLine(sum);
+
+
+            #endregion
+
+
+
+Console.Read();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
